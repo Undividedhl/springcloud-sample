2b23ae2b61a99c8084cead93375afbd8edb3dc79
coriander <1019958984@qq.com>
2020-08-05 11:59:17 +0800

    init

---
+++ springcloud-001/eureka-server/src/main/java/com/suckmydisk/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,27 @@
+package com.suckmydisk.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * main class
+ * 启动一个服务注册中心，只需要在此添加一个注解@EnableEurekaServer
+ * <p>
+ * eureka是一个高可用的组件，它没有后端缓存
+ * 每一个实例注册之后需要向注册中心发送心跳（因此可以在内存中完成）
+ * 在默认情况下erureka server也是一个eureka client ,必须要指定一个 server。
+ *
+ * @author 10199
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
